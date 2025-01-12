aee019d9773b4171dbc031c8edf178d1fda0c987
sampalu <andreluisampaio@gmail.com>
2025-01-12 10:13:42 -0300

    configuracao inicial program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -7,36 +7,35 @@ var builder = WebApplication.CreateBuilder();
 builder.Host
     .UseOrleans(siloBuilder =>
     {
-        siloBuilder.UseLocalhostClustering();
         siloBuilder.Configure<ClusterOptions>(options =>
         {
             options.ClusterId = "dev";
             options.ServiceId = "dev-enrichment-service";
         });
-        //siloBuilder.AddDynamoDBGrainStorageAsDefault(op =>
-        //{
-        //    op.TableName = "dev-orleans-state";
-        //    op.Service = config.GetValue<string>("aws:region");
-        //});
-        //siloBuilder.UseDynamoDBClustering(op =>
-        //{
-        //    op.TableName = "dev-orleans-cluster";
-        //    op.Service = config.GetValue<string>("aws:region");
-        //});
-        //siloBuilder.Configure<EndpointOptions>(options =>
-        //{
-        //    // since we are using awsvpc each container gets its own dns and ip
-        //    var ip = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result.First();
-        //    options.AdvertisedIPAddress = ip;
-        //    // These 2 ports will be used by a cluster
-        //    // for silo to silo communications
-        //    options.SiloPort = EndpointOptions.DEFAULT_SILO_PORT;
-        //    // Port to use for the gateway (client to silo)
-        //    options.GatewayPort = EndpointOptions.DEFAULT_GATEWAY_PORT;
-        //    // Internal ports which you expose to docker
-        //    options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_GATEWAY_PORT);
-        //    options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_SILO_PORT);
-        //});
+        siloBuilder.AddDynamoDBGrainStorageAsDefault(op =>
+        {
+            op.TableName = "dev-orleans-state";
+            op.Service = "us-east-1";
+        });
+        siloBuilder.UseDynamoDBClustering(op =>
+        {
+            op.TableName = "dev-orleans-cluster";
+            op.Service = "us-east-1";
+        });
+        siloBuilder.Configure<EndpointOptions>(options =>
+        {
+            // since we are using awsvpc each container gets its own dns and ip
+            var ip = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result.First();
+            options.AdvertisedIPAddress = ip;
+            // These 2 ports will be used by a cluster
+            // for silo to silo communications
+            options.SiloPort = EndpointOptions.DEFAULT_SILO_PORT;
+            // Port to use for the gateway (client to silo)
+            options.GatewayPort = EndpointOptions.DEFAULT_GATEWAY_PORT;
+            // Internal ports which you expose to docker
+            options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_GATEWAY_PORT);
+            options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_SILO_PORT);
+        });
         siloBuilder.UseDashboard(options => { options.Port = 8081; });
     })    
     .ConfigureServices(
